bc40bd2b12fc8b5b071e1067a633a9a9b5a14ffa
Camilo Parra <caeparra@gmail.com>
2019-08-07 09:59:57 -0600

    [KIVA] fixed dockerfile and renamed package to be accepted in pypi
    Signed-off-by: Camilo Parra <caeparra@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(
-    name='ursa',
+    name='ursacl',
     version='0.1.0',
     url='https://github.com/cam-parra/python-ursa',
     description='python wrapper for ursa universal crypto library',
