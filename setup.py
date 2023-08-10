d6b6358747df0d1563a0171961ee9b50e023b1d4
Andrew Whitehead <cywolf@gmail.com>
2023-08-10 09:24:23 -0700

    update package metadata
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- setup.py
+++ setup.py
@@ -5,21 +5,20 @@ with open(os.path.abspath("./README.md"), "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='indy-bls',
-    version='0.1.1',
+    name="indy_bls",
+    version="0.1.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/hyperledger/indy-bls-wrapper-python',
+    url="https://github.com/hyperledger/indy-bls-wrapper-python",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    description='python wrapper for the indy bls signatures rust implementation',
-    license='Apache-2.0',
-    author='Andrew Whitehead, Berend Sliedrecht, Stephen Curran',
-    author_email='cywolf@gmail.com, berend@animo.id, swcurran@cloudcompass.ca',
-    packages=['indy-bls'],
-    install_requires=['pytest'],
-    tests_require=['pytest']
+    description="Python wrapper for the Indy BLS signatures implementation",
+    license="Apache-2.0",
+    author="Andrew Whitehead, Berend Sliedrecht, Stephen Curran",
+    author_email="cywolf@gmail.com, berend@animo.id, swcurran@cloudcompass.ca",
+    packages=["indy_bls"],
+    tests_require=["pytest"],
 )
